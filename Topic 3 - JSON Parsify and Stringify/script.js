5ce99cf5432fe369cbc862ae09888666916c1a9b
leger-brent <46615243+leger-brent@users.noreply.github.com>
2020-02-23 13:41:17 -0700

    update

--- Topic 3 - JSON Parsify and Stringify/script.js
+++ Topic 3 - JSON Parsify and Stringify/script.js
@@ -1,21 +1,74 @@
-//Parsing JSON as a variable
-//Create a variable that holds JSON formatted data
-var Input = '[{"Name":"Vahn", "HP": 50},{"Name":"Noa", "HP": 40}]';
-//Create a new variable to hold the parsed data of the previous variable
-var Parsed = JSON.parse(Input);
-//Example showing you can add more objects to the Parsed variable
-Parsed.push({ Name: "Gala", HP: 75 });
-//Create variable that turns a JavaScript Object into JSON
-var Output = JSON.stringify(Parsed);
-//Print the new JSON that should have our new addition "Gala" now as part of the JSON data
-console.log(Output);
+// Here I'm simulating a JSON file that you would import from an external webserver. I've created objects at the top level with objects nested underneath them
+var jsonInput = 
+`[
+   {
+      "name":"Vahn",
+      "level":15,
+      "hp":700,
+      "mp":90,
+      "atk":50,
+      "def":50,
+      "spd":50,
+      "raSeru":[
+         {
+            "name":"Meta",
+            "level":3
+         }
+      ]
+   },
+   {
+      "name":"Noa",
+      "level":12,
+      "hp":550,
+      "mp":75,
+      "atk":30,
+      "def":30,
+      "spd":70,
+      "raSeru":[
+         {
+            "name":"Terra",
+            "level":2
+         }
+      ]
+   }
+]`
 
-//Parsing RAW JSON
-//Create a variable that holds the data of the parsed function performed on JSON
-var Parse = JSON.parse('[{"Name":"Meta", "HP": 50},{"Name":"Terra", "HP": 40}]');
-//Same as above
-Parse.push({ Name: "Ozma", HP: 75 });
-//Same as above
-var Output1 = JSON.stringify(Parse);
-//Print the line to the console.
-console.log(Output1);
+//I print the simulated input to the log to verify that it actually a string file.
+console.log(jsonInput);
+
+//Now that I've verifed my JSON is actually a string, I'm ready to parse it. Parsing it will allow me to manipulate the data of the object using functions. 
+var parsed = JSON.parse(jsonInput);
+
+//Now that the object is parsed, I can use a function. I use the push function to add another hero to the array.
+parsed.push(
+[
+   {
+      "name":"Gala",
+      "level":10,
+      "hp":700,
+      "mp":40,
+      "atk":70,
+      "def":60,
+      "spd":20,
+      "raSeru":[
+         {
+            "name":"Ozma",
+            "level":1
+         }
+      ]
+   }
+   ]
+   )
+
+//I can also manipulate a specific data field by referencing the index of the object in the array and using the dot notation on the field. 
+//Here I update the hp for my first hero.
+parsed[0].hp = parsed[0].hp + 100;
+
+//I print the updated value to the console to verify that it updated.
+console.log(parsed[0].hp);
+
+//To turn my object back into a string, I use the stringify function on my parsed variable. I chose to create a new variable to hold the new string data.
+var jsonOutput = JSON.stringify(parsed);
+
+//I print the new variable to the console and see that it is indeed a string and no longer an object.
+console.log(jsonOutput);
