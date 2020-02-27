e774afbf8a70e9335fa602a2312f1430c22fb0c2
leger-brent <46615243+leger-brent@users.noreply.github.com>
2020-02-27 16:08:51 -0700

    update

--- Topic 9 - JavaScript Events/script.js
+++ Topic 9 - JavaScript Events/script.js
@@ -1,13 +1,14 @@
 window.onload = function () {
 // onTouchBegin, onLoad, etc.)
 
-    document.getElementById("square").ontouchstart = function () {
-        document.getElementById("square").style.animationPlayState = "running"
-    };
 
-    document.getElementById("square").addEventListener("touchend",ontouchend,false);{
-        document.getElementById("square").style.animationPlayState = "paused"
-    };
+    document.getElementById("square").addEventListener("touchstart", function () {
+        if (document.getElementById("square").style.animationPlayState === "running") {
+            document.getElementById("square").style.animationPlayState = "paused"
+        } else {
+            document.getElementById("square").style.animationPlayState = "running"
+        }
+    });
 
 
 }
